ef5c84635dafa8ee538875af3a762135b439fd71
robEsq <robert.esquillon@studytafensw.edu.au>
2024-05-15 10:14:53 +1000

    Added JSDoc to PizzaCalculator. Removed unused files.

--- Submission_Part3_Robert_ESQUILLON/Pizza Order Form/script.js
+++ Submission_Part3_Robert_ESQUILLON/Pizza Order Form/script.js
@@ -1,6 +1,17 @@
+/**
+ * @function
+ * @name calculatePizzas
+ * @description This function calculates the number of pizzas inputted by the user
+ *  1 pizza     = 6.45
+ *  2 pizzas    = 14.00
+ *  3 pizzas    = 12.00
+ *  4 or more   = combination of the above
+ *  This will call everytime the value of the text box is changed
+ */
 function calculatePizzas() {
-    let pizzas = document.getElementById("pizza_count").value;
+    const pizzas = document.getElementById("pizza_count").value;
 
+    // First checks if the input is a number and not a negative
     if (!isNaN(pizzas) && pizzas >= 0) {
         let totalCost = 0;
         totalCost = parseInt((pizzas / 3)) * 14;
